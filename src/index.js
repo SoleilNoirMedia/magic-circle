a4e51a8bf5f178194e6ccbec3ef0b63bf79014c2
Juan M. Garcia <dev@soleilnoirmedia.com>
2017-03-22 02:06:09 -0400

    Created basic project configuration.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Layout from './components/layout/layout';
+import './index.css';
+
+ReactDOM.render(
+  <Layout />,
+  document.getElementById('root')
+);
